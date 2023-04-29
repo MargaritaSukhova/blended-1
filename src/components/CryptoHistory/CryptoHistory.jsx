fc4c75b3164e87ac79c8d62b9556cd27a3db4ca1
Danil Deineka <tripledpadik@gmail.com>
2023-04-29 12:52:02 +0300

    task-4

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,39 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import {dateNormalizer} from 'utils/format-date'
+import PropTypes from "prop-types";
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({ items }) => {
+  return (
+    <BaseTable>
+      <THead>
+        <tr>
+          <Th>№</Th>
+          <Th>PRICE</Th>
+          <Th>AMOUNT</Th>
+          <Th>DATE</Th>
+        </tr>
+      </THead>
+
+      <tbody>
+        {items.map((item, index) => (
+          <Tr key={item.id}>
+            <Td>{index + 1}</Td>
+            <Td>{item.price}</Td>
+            <Td>{item.amount}</Td>
+            <Td>{dateNormalizer(item.date)}</Td>
+          </Tr>
+        ))}
+      </tbody>
+    </BaseTable>
+  );
 };
+
+
+CryptoHistory.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    amount: PropTypes.number.isRequired,
+    date: PropTypes.string.isRequired,
+  })).isRequired
+}
--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -1,5 +1,9 @@
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, format } from 'date-fns';
 
 export const formatDate = date => {
   return formatDistanceToNow(new Date(date), { addSuffix: true });
 };
+
+export const dateNormalizer = date => {
+  return format(new Date(date), "Pp")
+}
